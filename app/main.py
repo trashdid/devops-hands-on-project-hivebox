5c3dc07ff3d17ee644e82d1919836489a2aa5f78
Siddharth Karunakaran <siddharthkarunakaran@gmail.com>
2026-05-22 16:53:20 -0400

    feat!: implemented the litestar framework for an api

--- app/main.py
+++ app/main.py
@@ -1,4 +1,21 @@
-from versioning import get_current_version
+import os
+
+from litestar import Litestar
+from litestar.openapi.config import OpenAPIConfig
+from litestar.openapi.plugins import ScalarRenderPlugin
+import uvicorn
+
+from versioning import VersioningController
+
+routes = [VersioningController]
+
+app = Litestar(route_handlers=routes,
+               openapi_config=OpenAPIConfig(
+                   title="Hivebox API",
+                   description="API to learn about DevOps",
+                   version=str(os.getenv("APP_VERSION")),
+                   render_plugins=[ScalarRenderPlugin()]
+               ))
 
 if __name__ == '__main__':
-    print(get_current_version())
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
